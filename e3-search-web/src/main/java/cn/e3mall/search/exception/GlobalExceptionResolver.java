d09efbcfba9f2c8c017bc03fcae03f59873570bb
zhangsan <zhangsan@qq.com>
2020-01-29 22:40:36 +0800

    day08-2

---
+++ e3-search-web/src/main/java/cn/e3mall/search/exception/GlobalExceptionResolver.java
@@ -0,0 +1,33 @@
+package cn.e3mall.search.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 全局异常处理器
+ */
+public class GlobalExceptionResolver implements HandlerExceptionResolver {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionResolver.class);
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object o, Exception e) {
+        //打印控制台
+        e.printStackTrace();
+        //写日志
+        logger.debug("测试输出的日志.....");
+        logger.info("系统发生异常了.......");
+        logger.error("系统发生异常",e);
+        //发邮件，短信
+        //使用jmail工具包...
+        //显示错误页面
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error/exception");
+        return modelAndView;
+    }
+}
